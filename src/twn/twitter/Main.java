294e7ccdb9ed64126414b9840645da9c7f0d5290
tywunon <tywunon@gmx.de>
2014-08-18 10:40:04 +0200

    - added twitter api

--- src/twn/twitter/Main.java
+++ src/twn/twitter/Main.java
@@ -1,10 +1,14 @@
 package twn.twitter;
 
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		TwitterFactory factory = new TwitterFactory();
+		Twitter twitter = factory.getInstance();
+		//do the magic stuff
 	}
 
 }
